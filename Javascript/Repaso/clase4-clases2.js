f70b6fe89c2e7435c2c572a49feab5d0db659643
OscarVargas97 <oscarvargas97@gmail.com>
2022-08-09 00:45:21 -0400

    Repaso de js terminado

--- Javascript/Repaso/clase4-clases2.js
+++ Javascript/Repaso/clase4-clases2.js
@@ -1,29 +1,53 @@
-const r = new Rectangulo()
+const r = new Rectangulo();
 
 class Chancho {
 	constructor(estado = 'normal') {
-		console.log(`soy un chancho ${estado}`)
-	}
-}
+		console.log(`soy un chancho ${estado}`);
+	};
+};
 
 class Chancho2 {
 	estado = 'normal'
 	constructor() {
-		console.log(`soy un chancho ${this.estado}`)
-	}
-}
+		console.log(`soy un chancho ${this.estado}`);
+	};
+};
 
 class Chancho3 {
 	estado = 'normal'
 	constructor(estado) {
 		this.estado = estado
-	}
+	};
 
 	hablar() {
+		console.log(`soy un chancho ${this.estado}`);
+	};
+};
 
-	}
-}
+class Chancho4 {
+	propiedad = 'mi propiedad'
+	#hambre = 'false'
+	constructor(estado) {
+		this.estado = estado
+	};
 
-const feliz = new Chancho('feliz')
-const triste = new Chancho('triste')
-const nose = new Chancho2('') 
+	hablar() {
+		console.log(`soy un chancho ${this.estado} ${this.#hambre ? 'con mucha hambre' : 'satisfecho'}`);
+	};
+};
+
+const feliz = new Chancho('feliz');
+const triste = new Chancho('triste');
+
+const nose = new Chancho2('');
+
+const feliz2 = new Chancho3('feliz');
+const triste2 = new Chancho3('triste');
+const nose2 = new Chancho3('');
+feliz2.hablar();
+triste2.hablar();
+nose2.hablar();
+
+const feliz3 = new Chancho4('feliz');
+console.log(feliz3.__proto__);
+feliz3.hablar()
